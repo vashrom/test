1c1490dc6ea106340e0dcc1ec710d27ab661af14
vashrom <vas-hrom@ukr.net>
2024-03-06 18:36:05 +0200

    Add main file to repo

---
+++ source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+bool function()
+{
+ return true;
+}
+
+void main()
+{
+
+  std::cout << "hello";
+  std::cout << "how r u?";
+  std::cout << "goodbye";
+
+
+function();
+ 
+}
